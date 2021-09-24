f8c7476719875f5ae8d3a0be5edf6b02cb98ea10
eomsunyoung <kuroich123@naver.com>
2021-09-24 13:19:34 +0900

    DB 파일소스

--- DBworks/deptemp_join.sql
+++ DBworks/deptemp_join.sql
@@ -1,3 +1,23 @@
+-- 사원 검색 --
+SELECT * FROM employee;
+
+-- 부서 검색 --
+SELECT * FROM department;
+
+-- 사원 추가 --
+INSERT INTO employee VALUES (102, '안산', 20, 20);
+INSERT INTO employee VALUES (101, '이강인', 21, 10);
+
+-- 사원 테이블 --
+CREATE TABLE employee(
+	empid  INTEGER,
+	name   TEXT NOT NULL,
+	age    INTEGER,
+	deptid INTEGER,
+	PRIMARY KEY(empid),
+	FOREIGN KEY(deptid) REFERENCES department(deptid)
+);
+
 -- 부서 추가 --
 INSERT INTO department VALUES (10, '관리팀', '서울');
 INSERT INTO department VALUES (20, '전산팀', '인천');
@@ -11,13 +31,6 @@ CREATE TABLE department(
 	PRIMARY KEY(deptid)
 );
 
-CREATE TABLE employee(
-	empid  INTEGER,
-	name   TEXT NOT NULL,
-	age	  INTEGER,
-	deptid INTEGER,
-	PRIMARY KEY(empid),
-	PRIMARY KEY(deptid) REFERENCES department(deptid)
-);
+
 
 COMMIT;
